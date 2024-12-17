ad958a4c9e311374579b7d8c2ca77ecb939b4ba9
Gabriel Hernanez <onedrakodev@gmail.com>
2024-12-16 23:52:27 -0300

    Seccion 02: Tipos básicos - Clase 7. Comentarios.

---
+++ 02-tipos/05-comentarios.js
@@ -0,0 +1,14 @@
+// qwer tyuiopwqe
+/*
+Todo lo que escribamos 
+entre estos símbolos 
+va a ser ignorado
+*/
+/**
+ * Este es un comentario
+ * de muchas
+ * líneas.
+ */
+let numero = 42; // Este es otro comentario.
+
+console.log(numero);
