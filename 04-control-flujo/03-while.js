73c9b3e7acb6b81121fa513357168a852c420a31
Gabriel Hernandez <onedrakodev@gmail.com>
2024-12-30 13:41:12 -0600

    Seccion 04: control de flujo - Clase 4. while.

---
+++ 04-control-flujo/03-while.js
@@ -0,0 +1,11 @@
+// Imprimir los números pares hasta llegar a 10.
+
+let i = 0;
+while(i < 10){
+    if(i % 2 == 0){
+        console.log("Número par", i);
+    }
+    i++
+}
+
+console.log("Fuera del while");
