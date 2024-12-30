e3a773f5352e160f4034ab895fca25f5faa51753
Gabriel Hernandez <onedrakodev@gmail.com>
2024-12-30 11:29:34 -0600

    Seccion 04: control de flujo - Clase 3. else.

---
+++ 04-control-flujo/02-else.js
@@ -0,0 +1,13 @@
+// if(condición){ 
+//     expresión;
+// }
+
+let edad = 10;
+
+if(edad > 17){
+    console.log("Usuario mayor de edad");
+} else if(edad > 13){
+    console.log("Usuario necesita estar acompañado de un adulto");
+} else {
+    console.log("Usuario no puede ingresar");
+}
