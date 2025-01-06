b3362e3af484b5f294824f30951e126875887400
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-06 17:07:14 -0600

    Sección 05: Ejercicios - Clase 07: Ejercicio 6.

---
+++ 05-ejercicios/06.js
@@ -0,0 +1,16 @@
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function cuantosPositivos(arr){
+    let cantidad = 0;
+
+    for (elemento of array){
+        if(elemento > 0){
+            cantidad++;
+        }
+    }
+    
+    return cantidad;
+}
+
+let resultado = cuantosPositivos(array);
+console.log(resultado);
