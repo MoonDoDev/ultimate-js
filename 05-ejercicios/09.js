14a325bd88f8bb0f465286c88515b4b984eff35c
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-07 16:59:50 -0600

    Sección 05: Ejercicios - Clase 10: Ejercicio 9.

---
+++ 05-ejercicios/09.js
@@ -0,0 +1,36 @@
+let pares = [
+    [1, { name: "Nicolas"}],
+    [2, { name: "Felipe"}],
+    [3, { name: "Chanchito"}],
+];
+
+let array = [
+    {
+        id: 1,
+        name: "Nicolas",
+    },
+    {
+        id: 2,
+        name: "Felipe",
+    },
+    {
+        id: 3,
+        name: "Chanchito",
+    }
+];
+
+function toCollection(arr){
+    let collection = [];
+
+    for (idx in arr){
+        let elemento = arr[idx];
+        collection[idx] = elemento[1];
+        collection[idx].id = elemento[0];
+    }
+
+    return collection;
+}
+
+let resultado = toCollection(pares);
+console.log(resultado);
+
