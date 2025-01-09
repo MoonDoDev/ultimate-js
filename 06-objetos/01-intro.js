1e2457ed2d439f5ebc98d06424ab7feb324b9edb
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-08 18:09:43 -0600

    Sección 06: Objetos - Clase 1: Introducción.

---
+++ 06-objetos/01-intro.js
@@ -0,0 +1,12 @@
+let user = {
+     email : 'nico@holamundo.io',
+     name : 'Nicolas',
+     direccion : {
+        calle: 'Queen st',
+        numero: 15,
+    },
+    active: true,
+    recuperarClave: function (){
+        console.log('Recuperando clave...')
+    },
+};
