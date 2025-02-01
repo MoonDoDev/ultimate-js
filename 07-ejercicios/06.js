2cad43910311e843f07d9c8823742724e2441b56
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-31 18:14:21 -0600

    Sección 07: Ejercicios - Clase 07. Crea copias.

---
+++ 07-ejercicios/06.js
@@ -0,0 +1,14 @@
+let obj1 = { id: 1, nombre: "chanchito" };
+
+function crearCopia(obj){
+    let copia = {};
+
+    for (let llave in obj) {
+        copia[llave] = obj[llave];
+    }
+
+    return copia;
+};
+
+let obj2 = crearCopia(obj1);
+console.log( {obj1: obj1, obj2: obj2} );
