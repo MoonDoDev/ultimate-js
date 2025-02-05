a01e564505914a21cc0dd807b93b998603609b7e
Gabriel Hernandez <onedrakodev@gmail.com>
2025-02-05 10:43:28 -0600

    Sección 08: arrays - Clase 03. Eliminar elementos.

---
+++ 08-arrays/02-eliminar.js
@@ -0,0 +1,10 @@
+const letras = ['a', 'b', 'c', 'd'];
+
+// const final = letras.pop();
+// console.log(final, letras);
+
+// const comienzo = letras.shift();
+// console.log(comienzo, letras);
+
+letras.splice(1, 2);
+console.log(letras);
