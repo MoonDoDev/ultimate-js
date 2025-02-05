47cd6dc3065ea3116988f37edadfeda0e501e720
Gabriel Hernandez <onedrakodev@gmail.com>
2025-02-05 14:54:58 -0600

    Sección 08: arrays - Clase 04. Buscar primitivos.

---
+++ 08-arrays/03-buscar-primitivos.js
@@ -0,0 +1,9 @@
+const letras = ['a', 'b', 1, 'c', 'd', 1];
+
+console.log(letras.indexOf("c"));
+console.log(letras.indexOf(1, 2));
+// console.log(letras.lastIndexOf(1));
+
+// console.log(letras.indexOf(1) !== -1);
+
+// console.log(letras.includes("c"));
