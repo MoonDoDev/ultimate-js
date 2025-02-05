37e27d3fe2c4e8059296c232113991fb52c1f2c2
Gabriel Hernandez <onedrakodev@gmail.com>
2025-02-05 17:04:07 -0600

    Sección 08: arrays - Clase 05. Fat arrow functions.

---
+++ 08-arrays/04-arrow-function.js
@@ -0,0 +1,12 @@
+// function hola() {
+//     return 'Hola Mundo';
+// }
+
+const hola = mensaje => mensaje + 'Hola Mundo';
+
+const hola2 = (mensaje) => {
+    return mensaje + 'Hola Mundo';
+}
+
+const resultado = hola("Chanchito Feliz");
+console.log(resultado);
