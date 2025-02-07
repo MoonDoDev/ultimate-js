5a640b988e823f624071b3566c8c05585f7cddde
Gabriel Hernandez <onedrakodev@gmail.com>
2025-02-06 18:23:01 -0600

    Sección 08: arrays - Clase 06. Buscar referencias.

---
+++ 08-arrays/05-buscar-referencias.js
@@ -0,0 +1,14 @@
+let usuarios = [
+    { id: 1, name: "Chanchito" },
+    { id: 2, name: "Feliz" }
+];
+
+// const resultado = usuarios.indexOf({ id: 1, name: "Chanchito" });
+
+// const resultado = usuarios.find(usuario =>
+//     usuario.name === "Chanchito");
+
+const resultado = usuarios.findIndex(usuario =>
+    usuario.name === "Chanchito");
+
+console.log(resultado)
